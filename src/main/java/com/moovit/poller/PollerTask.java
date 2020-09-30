a40e76c35ffebcf4bfb684d9bbc38d50f6baea6e
Zvika Ruabnenko <zvika.rubanenko@arm.com>
2020-09-30 16:53:02 +0300

    Update PollerTask.java
    update poller task to signal the latch

--- src/main/java/com/moovit/poller/PollerTask.java
+++ src/main/java/com/moovit/poller/PollerTask.java
@@ -3,18 +3,21 @@ package com.moovit.poller;
 import lombok.AllArgsConstructor;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
 
 @AllArgsConstructor
 public class PollerTask extends Thread {
     private final String lineNumber;
     private final INextBusProvider provider;
     private final BlockingQueue<StopEtaLine> stopEtaQueue;
+    private final CountDownLatch countDownLatch;
 
 
     public void run() {
         for (StopEta stopEta : provider.getLineEta(lineNumber)) {
             stopEtaQueue.add(new StopEtaLine(stopEta, lineNumber));
         }
+        countDownLatch.countDown();
     }
 
 }
